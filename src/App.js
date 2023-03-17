da61a1c99dc0dfbacc21cf1d1d902d00829bcb15
abdu-tech2023 <abdul20297k@gmail.com>
2023-03-17 10:07:54 -0500

    apple react first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import "./CommonResoucre/css/styles.css"
+import "./CommonResoucre/css/bootstrap.css"
+import Header from './Components/Header/Header';
+import Alert from './Components/Alert/Alert';
+import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
+import YoutubeVideo from './Components/YoutubeVideo/YoutubeVideo';
+
+
+function App() {
+    <div className="App">
+      <Header />
+      <Alert />
+      <Main />
+      <YoutubeVideo></YoutubeVideo>
+      <Footer/>
+
+
+
+
+      
+    </div>
+  );
+}
+
+export default App;
